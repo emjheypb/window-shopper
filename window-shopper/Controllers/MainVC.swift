9bc0705e262f913e5b33c2b3aa0f5bbd5d1bc5bc
Mariah Baysic <mjpb123@gmail.com>
2020-03-24 11:52:10 +0800

    added calculate logic

--- window-shopper/Controllers/MainVC.swift
+++ window-shopper/Controllers/MainVC.swift
@@ -12,11 +12,13 @@ class MainVC: UIViewController {
 
     @IBOutlet weak var wageTxt: CurrencyTextField!
     @IBOutlet weak var priceTxt: CurrencyTextField!
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var hoursLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 60))
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
         calcBtn.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
         calcBtn.setTitle("Calculate", for: .normal)
         calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
@@ -24,11 +26,33 @@ class MainVC: UIViewController {
         
         wageTxt.inputAccessoryView = calcBtn
         priceTxt.inputAccessoryView = calcBtn
+        
+        wageTxt.text = ""
+        priceTxt.text = ""
+        
+        hideLabels(true)
     }
     
     @objc func calculate() {
-        
+        if let wageTxt = wageTxt.text, let priceTxt = priceTxt.text {
+            if let wage = Double(wageTxt), let price = Double(priceTxt) {
+                view.endEditing(true)
+                resultLbl.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+                hideLabels(false)
+            }
+        }
     }
     
+    @IBAction func clearCalculatorPressed(_ sender: Any) {
+        wageTxt.text = ""
+        priceTxt.text = ""
+        
+        hideLabels(true)
+    }
+    
+    func hideLabels(_ hide: Bool) {
+        resultLbl.isHidden = hide
+        hoursLbl.isHidden = hide
+    }
 }
 
