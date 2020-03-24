5d83fafebfd140ba16113db4638aca509f496b00
Mariah Baysic <mjpb123@gmail.com>
2020-03-24 12:08:55 +0800

    added $ icon on currency text fields

--- window-shopper/Views/CurrencyTextField.swift
+++ window-shopper/Views/CurrencyTextField.swift
@@ -10,6 +10,24 @@ import UIKit
 
 @IBDesignable
 class CurrencyTextField: UITextField {
+    
+    override func draw(_ rect: CGRect) {
+        let size : CGFloat = 20
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.6538693905, green: 0.6499848366, blue: 0.6568568349, alpha: 0.8)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.text = formatter.currencySymbol
+        currencyLbl.clipsToBounds = true
+        
+        addSubview(currencyLbl)
+    }
 
     override func prepareForInterfaceBuilder() {
         customizeView()
@@ -27,6 +45,7 @@ class CurrencyTextField: UITextField {
         textAlignment = .center
         textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         keyboardType = .decimalPad
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
