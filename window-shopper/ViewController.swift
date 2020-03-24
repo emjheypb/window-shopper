7e9efd67d8bc9eb9c7d1d76ce3e9b69ae0d15b8f
Mariah Baysic <mjpb123@gmail.com>
2020-03-24 09:49:17 +0800

    Initial Commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Mariah Baysic on 3/24/20.
+//  Copyright © 2020 SpacedOut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
